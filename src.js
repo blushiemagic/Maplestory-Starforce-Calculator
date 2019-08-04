78fa8af369da530657858fdefe30a40c225956d2
blushiemagic <kayleekim96@gmail.com>
2019-08-04 12:05:31 -0700

    Initial commit

---
+++ src.js
@@ -0,0 +1,232 @@
+'use strict';
+
+const events = {
+    none: 0,
+    discount: 1,
+    guarantee: 2,
+    extra: 4
+}
+
+const mvp = {
+    none: 1,
+    silver: 0.97,
+    gold: 0.95,
+    diamond: 0.9
+}
+
+const destroyStar = 12;
+
+const data = {
+    0: { success: 0.95, drop: false },
+    1: { success: 0.9, drop: false },
+    2: { success: 0.85, drop: false },
+    3: { success: 0.85, drop: false },
+    4: { success: 0.8, drop: false },
+    5: { success: 0.75, drop: false },
+    6: { success: 0.7, drop: false },
+    7: { success: 0.65, drop: false },
+    8: { success: 0.6, drop: false },
+    9: { success: 0.55, drop: false },
+    10: { success: 0.5, drop: false },
+    11: { success: 0.45, drop: true },
+    12: { success: 0.4, drop: true, destroy: 0.01, safeguard: true },
+    13: { success: 0.35, drop: true, destroy: 0.02, safeguard: true },
+    14: { success: 0.3, drop: true, destroy: 0.02, safeguard: true },
+    15: { success: 0.3, drop: false, destroy: 0.03, safeguard: true },
+    16: { success: 0.3, drop: true, destroy: 0.03, safeguard: true },
+    17: { success: 0.3, drop: true, destroy: 0.03, safeguard: false },
+    18: { success: 0.3, drop: true, destroy: 0.04, safeguard: false },
+    19: { success: 0.3, drop: true, destroy: 0.04, safeguard: false },
+    20: { success: 0.3, drop: false, destroy: 0.1, safeguard: false },
+    21: { success: 0.3, drop: true, destroy: 0.1, safeguard: false },
+    22: { success: 0.03, drop: true, destroy: 0.2, safeguard: false },
+    23: { success: 0.02, drop: true, destroy: 0.3, safeguard: false },
+    24: { success: 0.01, drop: true, destroy: 0.4, safeguard: false }
+};
+
+function getPrice(args, star) {
+    var level = Math.floor(args.level / 10) * 10;
+    var base = 1000;
+    if (star < 10) {
+        base += Math.pow(level, 3) * (star + 1) / 25;
+    } else if (star < 15) {
+        base += Math.pow(level, 3) * Math.pow(star + 1, 2.7) / 400;
+    } else if (star < 18) {
+        base += Math.pow(level, 3) * Math.pow(star + 1, 2.7) / 120;
+    } else if (star < 20) {
+        base += Math.pow(level, 3) * Math.pow(star + 1, 2.7) / 110;
+    } else {
+        base += Math.pow(level, 3) * Math.pow(star + 1, 2.7) / 100;
+    }
+    var multiplier = args.mvpDiscount;
+    if ((args.event & events.discount) > 0) {
+        multiplier *= 0.7;
+    }
+    if (args.safeguard && data[star].safeguard) {
+        multiplier += 1;
+    }
+    var price = base * multiplier;
+    return Math.round(price / 100) * 100;
+}
+
+function getSuccessRate(args, star) {
+    var success = data[star].success;
+    if (args.catcher) {
+        success *= 1.045;
+    }
+    if ((args.event & events.guarantee) > 0 && (star === 5 || star === 10 || star === 15)) {
+        success = 1;
+    }
+    return success;
+}
+
+function calculateRange(from, to, results) {
+    var result = {
+        price: 0,
+        destroys: 0,
+        noDestroyChance: 1
+    };
+    for (var k = from; k < to; k++) {
+        result.price += results[k].price;
+        result.destroys += results[k].destroys;
+        result.noDestroyChance *= results[k].noDestroyChance;
+    }
+    return result;
+}
+
+function calculateStep(args, star, results) {
+    var price = getPrice(args, star);
+    var success = getSuccessRate(args, star);
+    var step = {};
+
+    //calculate cost of failure
+    //given: already 1 failure
+    var failureTable = [];
+    var remainingRate = 1;
+    var entry;
+    if (data[star].destroy && !(args.safeguard && data[star].safeguard)) {
+        //scenario: failure is a destroy
+        entry = {
+            weight: remainingRate * data[star].destroy,
+            price: price,
+            destroys: 1,
+            noDestroyChance: 0
+        };
+        if (star > destroyStar) {
+            var range = calculateRange(destroyStar, star, results);
+            entry.price += range.price;
+            entry.destroys += range.destroys;
+        }
+        failureTable.push(entry);
+        remainingRate -= entry.weight;
+    }
+    if (data[star].drop) {
+        //scenario: failure is a drop
+        if (data[star - 1].drop) {
+            //if (we can still drop more)
+            //scenario: success immediately after failing
+            var dropPrice = getPrice(args, star - 1);
+            var dropSuccess = getSuccessRate(args, star - 1);
+            entry = {
+                weight: remainingRate * dropSuccess,
+                price: dropPrice + price,
+                destroys: 0,
+                noDestroyChance: 1
+            };
+            failureTable.push(entry);
+            remainingRate -= entry.weight;
+            if (data[star - 1].destroy && !(args.safeguard && data[star - 1].safeguard)) {
+                //scenario: destroy right after failing
+                entry = {
+                    weight: remainingRate * data[star - 1].destroy,
+                    price: dropPrice + price,
+                    destroys: 1,
+                    noDestroyChance: 0
+                };
+                if (star > destroyStar) {
+                    var range = calculateRange(destroyStar, star, results);
+                    entry.price += range.price;
+                    entry.destroys += range.destroys;
+                }
+                failureTable.push(entry);
+                remainingRate -= entry.weight;
+            }
+            //scenario: drop a second time and activate chance time
+            entry = {
+                weight: remainingRate,
+                price: getPrice(args, star - 2) + dropPrice + price + results[star - 1].price,
+                destroys: results[star - 1].destroys,
+                noDestroyChance: results[star - 1].noDestroyChance
+            };
+            failureTable.push(entry);
+            remainingRate -= entry.weight;
+        } else {
+            //if (we can't drop anymore)
+            entry = {
+                weight: remainingRate,
+                price: price + results[star - 1].price,
+                destroys: results[star - 1].destroys,
+                noDestroyChance: results[star - 1].noDestroyChance
+            };
+            failureTable.push(entry);
+            remainingRate -= entry.weight;
+        }
+    } else {
+        //scenario: failure is a keep
+        entry = {
+            weight: remainingRate,
+            price: price,
+            destroys: 0,
+            noDestroyChance: 1
+        };
+        failureTable.push(entry);
+        remainingRate -= entry.weight;
+    }
+    console.log(star);
+    console.log(failureTable);
+    var failurePrice = 0;
+    var failureDestroys = 0;
+    var destroyChance = 0;
+    for (var k = 0; k < failureTable.length; k++) {
+        var entry = failureTable[k];
+        failurePrice += entry.weight * entry.price;
+        failureDestroys += entry.weight * entry.destroys;
+        destroyChance += entry.weight * (1 - entry.noDestroyChance);
+    }
+
+    //calculate average cost
+    var numFailures = (1 - success) / success;
+    step.price = price + failurePrice * numFailures;
+    step.destroys = failureDestroys * numFailures;
+    step.noDestroyChance = Math.pow(1 - destroyChance, numFailures);
+
+    results[star] = step;
+    return step;
+}
+
+function calculate() {
+    var args = {};
+    args.event = events[document.getElementById('event').value];
+    args.mvpDiscount = mvp[document.getElementById('mvp').value];
+    args.level = parseInt(document.getElementById('level').value);
+    var from = parseInt(document.getElementById('from').value);
+    var to = parseInt(document.getElementById('to').value);
+    args.safeguard = document.getElementById('safeguard').checked;
+    args.catcher = document.getElementById('catcher').checked;
+    var results = [];
+    for (var k = 0; k < to; k++) {
+        calculateStep(args, k, results);
+    }
+    var result = calculateRange(from, to, results);
+    var resultDiv = document.getElementById('results');
+    resultDiv.innerHTML = '';
+    var div = document.createElement('div');
+    div.innerHTML = 'Average Cost: ' + result.price.toLocaleString() + ' mesos';
+    resultDiv.appendChild(div);
+    div = document.createElement('div');
+    div.innerHTML = 'Average Destroys: ' + result.destroys;
+    resultDiv.appendChild(div);
+    div = document.createElement('div');
+    div.innerHTML = 'Chance of Destruction: ' + ((1 - result.noDestroyChance) * 100).toLocaleString() + '%';
+    resultDiv.appendChild(div);
+}
